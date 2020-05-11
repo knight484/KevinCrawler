f8116af37d0a9a18562f02f7ac19ca3670d7be69
kevin-chaos <zhaoyinjing9@hotmail.com>
2020-05-11 13:17:20 +0800

    严道医声 网站爬虫

--- DrVoice.py
+++ DrVoice.py
@@ -47,6 +47,7 @@ class DrVoice:
                                                item.find('div', class_='course-list-item-label').text).group(1)
                 course['title'] = item.find('div', class_='course-list-item-description-container').text.strip()
                 course['uid'] = item['href'].split('/')[-1]
+                course['post_link'] = item.find('img')['src']
                 t1 = time.time()
                 table.update_one({"uid": course["uid"]}, {"$set": course}, True)
                 end = time.time()
@@ -178,6 +179,42 @@ class DrVoice:
             n += 1
         return None
 
+    def get_doctor_info(self, n_list):
+        table = self.db['医生信息']
+        table.create_index('uid')
+
+        start = time.time()
+        for e, n in enumerate(n_list):
+            url = f'https://www.drvoice.cn/user/{n}'
+            r = req_get(url, header=self.header, proxy=self.proxy)
+            soup = BeautifulSoup(r.text, 'lxml')
+            doctor = dict()
+            doctor['uid'] = n
+            doctor['url'] = f'https://www.drvoice.cn/user/{n}'
+            try:
+                doctor['姓名'] = soup.find('div', class_="doctor-author").find('a').text.strip()
+            except AttributeError:
+                continue
+            try:
+                doctor['医院'] = soup.find('div', class_='doctor-hospital').text.strip()
+            except AttributeError:
+                pass
+            try:
+                doctor['简介'] = soup.find('div', class_="doctor-info").text.strip()
+            except AttributeError:
+                pass
+            t1 = time.time()
+            table.update_one({"uid": doctor["uid"]}, {"$set": doctor}, True)
+            end = time.time()
+            spend = end - start
+            unit_spend = spend / (e + 1)
+            remain = (len(n_list) - (e + 1)) * unit_spend
+            print(f"\r进度({(e + 1)}/{len(n_list)}), 本次数据存储用时{round(end - t1, 4)}秒"
+                  f"用时{int(spend // 3600)}:{int(spend % 3600 // 60)}:{int(spend % 60)}, "
+                  f"预计还剩{int(remain // 3600)}:{int(remain % 3600 // 60)}:{int(remain % 60)}.", end='')
+
+        return None
+
     def get_news_info(self, url_list):
         table = self.db['文章信息']
         table.create_index('uid')
@@ -220,5 +257,7 @@ if __name__ == "__main__":
     c = 'Hm_lvt_f5b5599fdcae9b1c564fa66762058be6=1588257401; sszztauth=6c4dDfPFniabW4xLOUxdnI2kGUWzSDFHGY42w5DRlLOq87AsCWm_m7Wlc0MOpivdd2vvgZNEJ4nzmYha-3z_4HVkqNROd24OS6OXw43kDVLAv_AoAl79AAw6BoomSptJqF31cQIqmzwd3-GLk_2tNg; sszzt_userid=7999PIYzhluoBzUviB4XRKZMFcf-frjzDOguWVoV4VevHmg; sszzt_username=51aeUjDCv7sDmJk8DjTNl-o1JPOcUVeAKvrtnAE8Y5XAtazNv_V1yGjT0Mk; sszzt_nickname=d20fQ1zS75I7ySnvPza-M8FYAyj04n21BokGJMDNb-mZHXY; sszzt_groupid=621a6OlD7Yd_y9soj9KYW4XRffF1DkWVdy0zqKl6; sszztcookietime=3bd7M29xc0VUhnvszneepcqtUWT880jMz6sIbhorst_F4qjYbAAn; Hm_lpvt_f5b5599fdcae9b1c564fa66762058be6=1588257710'
     h = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'
     dr = DrVoice(headers=h, cookies=c)
-    urls = dr.db['文章网址'].distinct('url')
-    dr.get_news_info(urls)
+    ids = dr.db['医生信息'].distinct('uid')
+    ids = [n for n in range(300000) if n not in ids]
+    ids = ids[:int(len(ids) / 2)]
+    dr.get_doctor_info(n_list=ids)
