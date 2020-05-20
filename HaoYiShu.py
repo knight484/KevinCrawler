6e2a559870e7062a46a9082aaf58e9aa657992b5
kevin-chaos <zhaoyinjing9@hotmail.com>
2020-05-20 12:03:05 +0800

    好医术 网站爬虫

--- HaoYiShu.py
+++ HaoYiShu.py
@@ -24,61 +24,199 @@ class HaoYiShu:
         table = self.db['公开课网址']
         table.create_index('uid')
 
-        url = 'https://www.haoyishu.org/web/course/openCourse'
+        url = 'https://www.haoyishu.org/web/video/open'
         r = req_get(url, header=self.header, proxy=self.proxy)
         soup = BeautifulSoup(r.text, 'lxml')
-        max_page = int(soup.find_all('li', class_='number')[-1].text)
+        max_type = len(soup.find('ul', class_='condition_list').find_all('li', style='display:;'))
 
         start = time.time()
         n = 0
-        for i in range(max_page):
-            url = f'https://www.haoyishu.org/api/video/open/list/403010/{i + 1}?sourceId=0&typeId=0&departmentId=0&website=true'
+        for j in range(1, max_type):
+            url = f'https://www.haoyishu.org/api/v2/open/list/0/1?sourceId=0&typeId=0&departmentId={j}&website=true'
             r = req_get(url=url, header=self.header, proxy=self.proxy)
             contents = demjson.decode(r.text)
-            videos = contents['data']['list']
+            max_page = contents['data']['pageCount']
 
-            for video in videos:
-                video['url'] = f'https://www.haoyishu.org/web/videoplay/open?albumId={video["albumId"]}'
-                video['uid'] = video['albumId']
-                table.update_one({"uid": video["uid"]}, {"$set": video}, True)
-                n += 1
-                end = time.time()
-                spend = end - start
-                unit_spend = spend / (i + 1 / max_page)
-                remain = (max_page - i - 1 / max_page) * unit_spend
-                print(f"\r进度({i + 1}/{max_page}), 获得{n}条视频数据, "
-                      f"用时{int(spend // 3600)}:{int(spend % 3600 // 60)}:{int(spend % 60)}, "
-                      f"预计还剩{int(remain // 3600)}:{int(remain % 3600 // 60)}:{int(remain % 60)}.", end='')
+            for i in range(max_page):
+                url = f'https://www.haoyishu.org/api/v2/open/list/0/{i + 1}?sourceId=0&typeId=0&departmentId={j}&website=true'
+                r = req_get(url=url, header=self.header, proxy=self.proxy)
+                contents = demjson.decode(r.text)
+                videos = contents['data']['list']
+
+                for video in videos:
+                    video['url'] = f'https://www.haoyishu.org/web/video/open/{video["albumId"]}'
+                    video['uid'] = video['albumId']
+                    video['departmentId'] = j
+                    del video['article']
+
+                    table.update_one({"uid": video["uid"]}, {"$set": video}, True)
+                    n += 1
+                    end = time.time()
+                    spend = end - start
+                    unit_spend = spend / (i + 1 / max_page)
+                    remain = (max_page - i - 1 / max_page) * unit_spend
+                    print(f"\r正在进行'第{j}类科室'的信息爬取，进度({i + 1}/{max_page}), 获得{n}条视频数据, "
+                          f"用时{int(spend // 3600)}:{int(spend % 3600 // 60)}:{int(spend % 60)}, "
+                          f"预计还剩{int(remain // 3600)}:{int(remain % 3600 // 60)}:{int(remain % 60)}.", end='')
 
     def get_vip_id(self):
         table = self.db['会员课网址']
         table.create_index('uid')
 
-        url = 'https://www.haoyishu.org/web/course/vipCourse'
+        url = 'https://www.haoyishu.org/web/video/vip'
         r = req_get(url, header=self.header, proxy=self.proxy)
         soup = BeautifulSoup(r.text, 'lxml')
-        max_page = int(soup.find_all('li', class_='number')[-1].text)
+        # max_page = int(soup.find_all('li', class_='number')[-1].text)
+        max_type = len(soup.find('ul', class_='condition_list').find_all('li'))
 
         start = time.time()
         n = 0
-        for i in range(max_page):
-            url = f'https://www.haoyishu.org/api/video/vip/list/403010/{i + 1}?sourceId=0&typeId=0&departmentId=0&website=true'
+        for j in range(1, max_type):
+            url = f'https://www.haoyishu.org/api/v2/vip/list/0/1?sourceId=0&typeId=0&departmentId={j}&website=true'
             r = req_get(url=url, header=self.header, proxy=self.proxy)
             contents = demjson.decode(r.text)
-            videos = contents['data']['list']
+            max_page = contents['data']['pageCount']
 
-            for video in videos:
-                video['url'] = f'https://www.haoyishu.org/web/videoplay/vip?albumId={video["albumId"]}'
-                video['uid'] = video['albumId']
-                table.update_one({"uid": video["uid"]}, {"$set": video}, True)
-                n += 1
-                end = time.time()
-                spend = end - start
-                unit_spend = spend / (i + 1 / max_page)
-                remain = (max_page - i - 1 / max_page) * unit_spend
-                print(f"\r进度({i + 1}/{max_page}), 获得{n}条视频数据, "
-                      f"用时{int(spend // 3600)}:{int(spend % 3600 // 60)}:{int(spend % 60)}, "
-                      f"预计还剩{int(remain // 3600)}:{int(remain % 3600 // 60)}:{int(remain % 60)}.", end='')
+            for i in range(max_page):
+                url = f'https://www.haoyishu.org/api/v2/vip/list/0/{i + 1}?sourceId=0&typeId=0&departmentId={j}&website=true'
+                r = req_get(url=url, header=self.header, proxy=self.proxy)
+                contents = demjson.decode(r.text)
+                videos = contents['data']['list']
+
+                for video in videos:
+                    video['url'] = f'https://www.haoyishu.org/web/video/vip/{video["albumId"]}'
+                    video['uid'] = video['albumId']
+                    video['departmentId'] = j
+                    del video['article']
+
+                    table.update_one({"uid": video["uid"]}, {"$set": video}, True)
+                    n += 1
+                    end = time.time()
+                    spend = end - start
+                    unit_spend = spend / (i + 1 / max_page)
+                    remain = (max_page - i - 1 / max_page) * unit_spend
+                    print(f"\r正在进行'第{j}类科室'的信息爬取，进度({i + 1}/{max_page}), 获得{n}条视频数据, "
+                          f"用时{int(spend // 3600)}:{int(spend % 3600 // 60)}:{int(spend % 60)}, "
+                          f"预计还剩{int(remain // 3600)}:{int(remain % 3600 // 60)}:{int(remain % 60)}.", end='')
+
+    def get_case_id(self):
+        table = self.db['案例课网址']
+        table.create_index('uid')
+
+        url = 'https://www.haoyishu.org/web/video/case'
+        r = req_get(url, header=self.header, proxy=self.proxy)
+        soup = BeautifulSoup(r.text, 'lxml')
+        # max_page = int(soup.find_all('li', class_='number')[-1].text)
+        max_type = len(soup.find('ul', class_='stateList').find_all('li'))
+
+        start = time.time()
+        n = 0
+        for j in range(1, max_type):
+            url = f'https://www.haoyishu.org/api/v2/case/list/0/1?sourceId=0&typeId=0&departmentId={j}&website=true'
+            r = req_get(url=url, header=self.header, proxy=self.proxy)
+            contents = demjson.decode(r.text)
+            max_page = contents['data']['pageCount']
+
+            for i in range(max_page):
+                url = f'https://www.haoyishu.org/api/v2/case/list/0/{i + 1}?sourceId=0&typeId=0&departmentId={j}&website=true'
+                r = req_get(url=url, header=self.header, proxy=self.proxy)
+                contents = demjson.decode(r.text)
+                videos = contents['data']['list']
+
+                for video in videos:
+                    video['url'] = f'https://www.haoyishu.org/web/video/case/{video["albumId"]}'
+                    video['uid'] = video['albumId']
+                    video['departmentId'] = j
+                    del video['article']
+
+                    table.update_one({"uid": video["uid"]}, {"$set": video}, True)
+                    n += 1
+                    end = time.time()
+                    spend = end - start
+                    unit_spend = spend / (i + 1 / max_page)
+                    remain = (max_page - i - 1 / max_page) * unit_spend
+                    print(f"\r正在进行'第{j}类科室'的信息爬取，进度({i + 1}/{max_page}), 获得{n}条视频数据, "
+                          f"用时{int(spend // 3600)}:{int(spend % 3600 // 60)}:{int(spend % 60)}, "
+                          f"预计还剩{int(remain // 3600)}:{int(remain % 3600 // 60)}:{int(remain % 60)}.", end='')
+
+    def get_meeting_id(self):
+        table = self.db['学习班网址']
+        table.create_index('uid')
+
+        url = 'https://www.haoyishu.org/web/meeting'
+        r = req_get(url, header=self.header, proxy=self.proxy)
+        soup = BeautifulSoup(r.text, 'lxml')
+        max_type = len(soup.find('section', class_='container').find_all('li', style='display:;')) + 1
+
+        start = time.time()
+        n = 0
+        for j in range(1, max_type):
+            url = f'https://www.haoyishu.org/api/meeting/past/0/1?departmentId={j}&website=true'
+            r = req_get(url=url, header=self.header, proxy=self.proxy)
+            contents = demjson.decode(r.text)
+            max_page = contents['data']['pageCount']
+
+            for i in range(max_page):
+                url = f'https://www.haoyishu.org/api/meeting/past/0/{i + 1}?departmentId={j}&website=true'
+                r = req_get(url=url, header=self.header, proxy=self.proxy)
+                contents = demjson.decode(r.text)
+                meetings = contents['data']['list']
+
+                for meeting in meetings[:3]:
+                    meeting['url'] = f'https://www.haoyishu.org/web/meeting/detail?meetingId={meeting["meetingId"]}'
+                    meeting['uid'] = meeting['meetingId']
+                    meeting['departmentId'] = j
+                    for k in meeting['album']:
+                        meeting['album' + k] = meeting['album'][k]
+                    del meeting['album']
+
+                    table.update_one({"uid": meeting["uid"]}, {"$set": meeting}, True)
+                    n += 1
+                    end = time.time()
+                    spend = end - start
+                    unit_spend = spend / (i + 1 / max_page)
+                    remain = (max_page - i - 1 / max_page) * unit_spend
+                    print(f"\r正在进行'第{j}类科室'的信息爬取，进度({i + 1}/{max_page}), 获得{n}条学习班数据, "
+                          f"用时{int(spend // 3600)}:{int(spend % 3600 // 60)}:{int(spend % 60)}, "
+                          f"预计还剩{int(remain // 3600)}:{int(remain % 3600 // 60)}:{int(remain % 60)}.", end='')
+
+    def get_article_id(self):
+        table = self.db['早读文章网址']
+        table.create_index('uid')
+
+        url = 'https://www.haoyishu.org/web/article'
+        r = req_get(url, header=self.header, proxy=self.proxy)
+        soup = BeautifulSoup(r.text, 'lxml')
+        max_type = len(soup.find('ul', class_='condition_list').find_all('li', style='display:;'))
+
+        start = time.time()
+        n = 0
+        for j in range(1, max_type):
+            url = f'https://www.haoyishu.org/api/article/list/1/0?website=true&departmentId={j}&tag='
+            r = req_get(url=url, header=self.header, proxy=self.proxy)
+            contents = demjson.decode(r.text)
+            max_page = contents['data']['pageCount']
+
+            for i in range(max_page):
+                url = f'https://www.haoyishu.org/api/article/list/{i + 1}/0?website=true&departmentId={j}&tag='
+                r = req_get(url=url, header=self.header, proxy=self.proxy)
+                contents = demjson.decode(r.text)
+                articles = contents['data']['list']
+
+                for article in articles[:3]:
+                    article['url'] = f'https://www.haoyishu.org/web/article/detail?articleId={article["articleId"]}'
+                    article['uid'] = article['articleId']
+                    article['departmentId'] = j
+
+                    table.update_one({"uid": article["uid"]}, {"$set": article}, True)
+                    n += 1
+                    end = time.time()
+                    spend = end - start
+                    unit_spend = spend / (i + 1 / max_page)
+                    remain = (max_page - i - 1 / max_page) * unit_spend
+                    print(f"\r正在进行'第{j}类科室'的信息爬取，进度({i + 1}/{max_page}), 获得{n}条文章数据, "
+                          f"用时{int(spend // 3600)}:{int(spend % 3600 // 60)}:{int(spend % 60)}, "
+                          f"预计还剩{int(remain // 3600)}:{int(remain % 3600 // 60)}:{int(remain % 60)}.", end='')
 
 
 if __name__ == "__main__":
@@ -86,5 +224,4 @@ if __name__ == "__main__":
     c = 'gr_user_id=df512dd0-f7c1-4210-a576-0e2ca3f3309c; grwng_uid=e9403df9-4d2d-46a8-8336-9f8927c61f69; Hm_lvt_b8d21f481877d7af7d19f0ee88d98f33=1588320918; 87f7e3c4c896ecf7_gr_session_id=a7014c54-1b75-4c79-8b9e-215a8cd065b8; 87f7e3c4c896ecf7_gr_session_id_a7014c54-1b75-4c79-8b9e-215a8cd065b8=true; sid=s%3AUGHMivjOzKRPETounjfpfupP1RnZWAqn.GaL3tsoFNkc9eL0xLiHRnGVg69uAsekT9psgJwlduOQ; Hm_lpvt_b8d21f481877d7af7d19f0ee88d98f33=1588479277'
 
     hys = HaoYiShu(headers=h, cookies=c)
-    hys.get_open_id()
-    hys.get_vip_id()
+    hys.get_article_id()
